4dbf4491888aaa74687517c5d4ff4ef7c253b804
Chayanin Kongsareekul <kang.chayanin@gmail.com>
2022-10-28 03:42:33 +0700

    Update 07_StrFile_23_File_Min_Max_Average.py

--- ComProg/grader/07_StrFile/07_StrFile_23_File_Min_Max_Average.py
+++ ComProg/grader/07_StrFile/07_StrFile_23_File_Min_Max_Average.py
@@ -1,16 +1,27 @@
+def file2list(filename):
+    file = open(filename, 'r')
+    file_list = []
+
+    while True:
+        s = file.readline().strip()
+        if len(s) == 0:
+            break
+        file_list.append(s)
+
+    return file_list
+
 scores = []
 
 filename, year = [x.strip() for x in input().split()]
 
-with open(filename, 'r') as f:
-    while True:
-        s = f.readline().strip()
-        if len(s) == 0:
-            break
-        student_id, score = [x.strip() for x in s.split()]
-        if student_id[:2] != year[2:]:
-            continue
-        scores.append(float(score))
+data = file2list(filename)
+
+for line in data:
+    student_id, score = [x.strip() for x in line.split()]
+
+    if student_id[: 2] != year[2 : ]:
+        continue
+    scores.append(float(score))
     
 if len(scores) == 0:
     print('No data')
